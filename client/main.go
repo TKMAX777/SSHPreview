4aa886847d6bbfc4a9b3b3e1bd5fa410edcac042
TKMAX777 <greatbananachips-service@yahoo.co.jp>
2021-05-04 02:26:20 +0900

    client side

---
+++ client/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"time"
+)
+
+func init() {
+	Settings.ListenPort = os.Getenv("PreviewListenPort")
+}
+
+func main() {
+
+	flag.Parse()
+	var args = flag.Args()
+
+	var req = NewRequestHandler("http://127.0.0.1:" + Settings.ListenPort)
+
+	interrupt := make(chan os.Signal, 1)
+
+	enter := make(chan bool, 1)
+
+	go func() {
+		for {
+			bufio.NewScanner(os.Stdin).Scan()
+			enter <- true
+		}
+	}()
+
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+
+	req.ChromeShow()
+
+	for _, p := range args {
+		f, err := os.Open(p)
+		if err != nil {
+			continue
+		}
+		fmt.Println(p)
+		req.Send(HTTPPreviewData{
+			Data:     f,
+			FileName: f.Name(),
+		})
+
+		f.Close()
+
+	wait:
+		for {
+			select {
+			case <-interrupt:
+				req.ChromeOff()
+				os.Exit(1)
+			case <-enter:
+				break wait
+			case <-ticker.C:
+			}
+		}
+	}
+	req.ChromeOff()
+
+}
---
+++ client/request.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"mime/multipart"
+	"net/http"
+)
+
+type RequestHandler struct {
+	client   *http.Client
+	localAdd string
+}
+
+type HTTPPreviewData struct {
+	Data     io.Reader
+	FileName string
+}
+
+type HTTPResponseFormat struct {
+	Status string
+}
+
+func NewRequestHandler(addr string) *RequestHandler {
+	return &RequestHandler{new(http.Client), addr}
+}
+
+func (r RequestHandler) ChromeShow() (err error) {
+	res, err := http.Get(r.localAdd + "/chromeshow")
+	if err != nil {
+		return
+	}
+	var b = new(bytes.Buffer)
+	io.Copy(b, res.Body)
+	fmt.Println(b)
+	fmt.Println(r.localAdd + "/chromeshow")
+	return
+
+}
+
+func (r RequestHandler) ChromeOff() (err error) {
+	http.Get(r.localAdd + "/chromeoff")
+	return
+}
+
+func (r *RequestHandler) Send(data HTTPPreviewData) (err error) {
+	var body = new(bytes.Buffer)
+	var mw = multipart.NewWriter(body)
+
+	fw, err := mw.CreateFormFile("file", data.FileName)
+	if err != nil {
+		return
+	}
+
+	io.Copy(fw, data.Data)
+	var contentType = mw.FormDataContentType()
+
+	mw.Close()
+
+	resp, err := http.Post(r.localAdd+"/remote", contentType, body)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+
+	var status HTTPResponseFormat
+	json.NewDecoder(resp.Body).Decode(&status)
+
+	if status.Status != "OK" {
+		fmt.Println(status.Status)
+	}
+
+	return
+}
